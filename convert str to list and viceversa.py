412f54888bc4a1fb5009a368c3ed761d4209bf75
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-24 11:13:19 +0530

    Added program to convert str to list and viceversa
    In this program we can convert a list to string and string to list.

---
+++ convert str to list and viceversa.py
@@ -0,0 +1,16 @@
+# Convert string to list without using list predefined function or any loops
+
+# String to list
+a = "guru"
+b = a.replace('',' ').split()
+print(a)
+print(b)
+
+print()
+
+
+# For list to string
+c = ['g','u','r','u']
+d = ''.join(c)
+print(c)
+print(d)
