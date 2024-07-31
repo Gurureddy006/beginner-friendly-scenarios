7cffea22c1f1ed597d1cd52aa7c4df8cf43acc3f
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-31 17:05:47 +0530

    Added smallest binary output
    find the smallest binary output from the given integer input and then convert it into binary and then again to integer.

---
+++ find the min binary output from the given int input.py
@@ -0,0 +1,12 @@
+# find the min binary output from the given integer input and conveert it into binary and then again to integer\
+
+a = int(input("Number:"))
+binary = bin(a)[2:]
+count_zero = binary.count('0')
+count_one = binary.count('1')
+
+new_bin = '0'* count_zero + '1' * count_one
+
+new_integer = int(new_bin,2)
+
+print("smallest integer :",new_integer)
