5b05a1de037ba600087bd70aded48695e6d1771e
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-31 17:06:12 +0530

    Update find the min binary output from the given int input.py

--- find the min binary output from the given int input.py
+++ find the min binary output from the given int input.py
@@ -2,11 +2,11 @@
 
 a = int(input("Number:"))
 binary = bin(a)[2:]
+
 count_zero = binary.count('0')
 count_one = binary.count('1')
 
 new_bin = '0'* count_zero + '1' * count_one
-
 new_integer = int(new_bin,2)
 
 print("smallest integer :",new_integer)
